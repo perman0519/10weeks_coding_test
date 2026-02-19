1473b387f76655960d425d019d9f59fc61a29675
junssong <xms2007@naver.com>
2026-02-19 17:37:06 +0900

    1940: 주몽 순열, 재귀 조합 재귀 없이

--- week1/1940.cpp
+++ week1/1940.cpp
@@ -40,5 +40,61 @@ int main() {
 	sort(num, num + n);
 	combi(n, 2, 0);
 	cout << ret;
-
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int n, m, a[15001], cnt;
+// int main() {
+// 	ios_base::sync_with_stdio(false);
+// 	cin.tie(0), cout.tie(0);
+// 	cin >> n >> m;
+// 	for (int i = 0; i < n; i++)
+// 		cin >> a[i];
+// 	if (m > 200000)
+// 		cout << 0 << "\n";
+// 	else {
+// 		for (int i = 0; i < n; i++) {
+// 			for (int j = 0; j < i; j++) {
+// 				if(a[i] + a[j] == m)
+// 					cnt++;
+// 			}
+// 		}
+// 		cout << cnt << '\n';
+// 	}
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int n, m, a[15001], cnt;
+// void combi(int idx, vector<int>& v){
+// 	if (v.size() == 2) {
+// 		int num1 = a[v[0]];
+// 		int num2 = a[v[1]];
+// 		if (num1 + num2 == m)cnt++;
+// 		return;
+
+// 	}
+// 	for (int i = idx + 1; i < n; i++) {
+// 		v.push_back(i);
+// 		combi(i, v);
+// 		v.pop_back();
+// 	}
+// }
+
+
+// int main() {
+// 	ios_base::sync_with_stdio(false);
+// 	cin.tie(0), cout.tie(0);
+// 	cin >> n >> m;
+// 	for (int i = 0; i < n; i++)
+// 		cin >> a[i];
+// 	if (m > 200000)
+// 		cout << 0 << "\n";
+// 	else {
+// 		vector<int> v;
+// 		combi(-1, v);
+// 		cout << cnt << '\n';
+// 	}
+// }
